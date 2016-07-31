cf1790c137bcf386a9082af1d56baddf7e09102b
Michael Manhire <michaelmanhire@gmail.com>
2016-07-31 16:14:34 -0700

    Add form validation for IE9

--- js/main.js
+++ js/main.js
@@ -523,13 +523,41 @@ if ( window.location.pathname == '/' || window.location.pathname == '/index.php'
 smoothScroll.init();
 
 var form = $("#form");
+
+function submitForm() {
+	var url = $(this).attr("action");
+	var formData = $(this).serialize();
+	$.post(url, formData, function() {
+		form.html('<p>Message received! Thanks!</p>');
+	});
+}
+
 form.submit(function(e) {
-    e.preventDefault();
-    var url = $(this).attr("action");
-    var formData = $(this).serialize();
-    $.post(url, formData, function() {
-       form.html('<p>Message received! Thanks!</p>');
-    });
+	e.preventDefault();
+
+	if ($('html').hasClass('no-formvalidation')) {
+		// Validation for outdated browsers
+		if ($('#email').val() == '' || $('#message').val() == '') {
+			$('form p').remove();
+			form.append('<p>Form could not be sent! Please ensure that both the email field and message field are filled out.</p>')
+		}
+		else {
+			$('form p').remove();
+
+			var url = $(this).attr("action");
+			var formData = $(this).serialize();
+			$.post(url, formData, function() {
+				form.html('<p>Message received! Thanks!</p>');
+			});
+		}
+	}
+	else {
+		var url = $(this).attr("action");
+		var formData = $(this).serialize();
+		$.post(url, formData, function() {
+			form.html('<p>Message received! Thanks!</p>');
+		});
+	}
 });
 
 //# sourceMappingURL=main.js.map
--- js/parts/form-submission.js
+++ js/parts/form-submission.js
@@ -1,9 +1,37 @@
 var form = $("#form");
-form.submit(function(e) {
-	e.preventDefault();
+
+function submitForm() {
 	var url = $(this).attr("action");
 	var formData = $(this).serialize();
 	$.post(url, formData, function() {
-	   form.html('<p>Message received! Thanks!</p>');
+		form.html('<p>Message received! Thanks!</p>');
 	});
+}
+
+form.submit(function(e) {
+	e.preventDefault();
+
+	if ($('html').hasClass('no-formvalidation')) {
+		// Validation for outdated browsers
+		if ($('#email').val() == '' || $('#message').val() == '') {
+			$('form p').remove();
+			form.append('<p>Form could not be sent! Please ensure that both the email field and message field are filled out.</p>')
+		}
+		else {
+			$('form p').remove();
+
+			var url = $(this).attr("action");
+			var formData = $(this).serialize();
+			$.post(url, formData, function() {
+				form.html('<p>Message received! Thanks!</p>');
+			});
+		}
+	}
+	else {
+		var url = $(this).attr("action");
+		var formData = $(this).serialize();
+		$.post(url, formData, function() {
+			form.html('<p>Message received! Thanks!</p>');
+		});
+	}
 });
